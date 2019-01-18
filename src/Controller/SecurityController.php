9194fab48798f04ffb19531e2d2dbd9fcc365f32
birnagerald <birnagerald.pro@gmail.com>
2019-01-18 05:33:32 +0100

    update: back && front register login logout system REBUILT

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,60 +2,23 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationType;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route ("/inscription", name="security_registration")
-     * @param Request $request
-     * @param ObjectManager $om
-     * @param UserPasswordEncoderInterface $encoder
+     * @Route("/connexion", name="app_login")
      */
-    
-    public function registration(Request $request, ObjectManager $om, UserPasswordEncoderInterface $encoder) 
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $user = new User;
-        $form = $this->createForm(RegistrationType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $hash = $encoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($hash);
-
-            $om->persist($user);
-            $om->flush();
-
-            return $this->redirectToRoute('security_login');
-        }
-
-        return $this->render('security/registration.html.twig',[
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route ("/connexion", name="security_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils)
-    {
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        dump($error);
-        return $this->render('security/login.html.twig', [
-            'error' => $error
-        ]);
-    }
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-    /**
-     * @Route ("/deconnexion", name="security_logout")
-     */
-    public function logout(){}
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
 }
