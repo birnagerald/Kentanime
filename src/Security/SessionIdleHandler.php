d980aefeba35a3ed25bd21b024d78525bd7c24ff
birnagerald <birnagerald.pro@gmail.com>
2019-01-23 19:30:58 +0100

    feat: back add logging out due to inactivity system

---
+++ src/Security/SessionIdleHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class SessionIdleHandler
+{
+
+    protected $session;
+    protected $securityToken;
+    protected $router;
+    protected $maxIdleTime;
+
+    public function __construct($maxIdleTime, SessionInterface $session, TokenStorageInterface $securityToken, RouterInterface $router)
+    {
+        $this->session = $session;
+        $this->securityToken = $securityToken;
+        $this->router = $router;
+        $this->maxIdleTime = $maxIdleTime;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST != $event->getRequestType()) {
+
+            return;
+        }
+
+        if ($this->maxIdleTime > 0) {
+
+            $this->session->start();
+            $lapse = time() - $this->session->getMetadataBag()->getLastUsed();
+
+            if ($lapse > $this->maxIdleTime) {
+
+                $this->securityToken->setToken(null);
+                $this->session->getFlashBag()->set('info', 'You have been logged out due to inactivity.');
+
+                // logout is defined in security.yaml.  See 'Logging Out' section here:
+                // https://symfony.com/doc/4.1/security.html
+                $event->setResponse(new RedirectResponse($this->router->generate(app_logout)));
+            }
+        }
+    }
+}
